96cf327ee97327e9a0f3103924a98898acaa30d0
Andrew Noble <aknoble.andrew@gmail.com>
2024-08-12 10:21:29 -0800

    Style ConfirmButton

--- frontend/src/components/private/ConfirmButton.jsx
+++ frontend/src/components/private/ConfirmButton.jsx
@@ -5,7 +5,7 @@ export default function ConfirmButton(props) {
   const { confirmAction, confirmText, initialText } = props;
 
   return (
-    <div>
+    <div className="p-4">
       {!confirm ? (
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
