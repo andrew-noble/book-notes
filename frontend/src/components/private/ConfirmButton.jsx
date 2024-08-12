9f999e01f3a4438fdab10cba127a2d0b9d169a75
Andrew Noble <aknoble.andrew@gmail.com>
2024-08-12 00:06:52 -0800

    Add ConfirmButton element

---
+++ frontend/src/components/private/ConfirmButton.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+export default function ConfirmButton(props) {
+  const [confirm, setConfirm] = useState(false);
+  const { confirmAction, confirmText, initialText } = props;
+
+  return (
+    <>
+      {!confirm ? (
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setConfirm(true)}
+        >
+          {initialText}
+        </button>
+      ) : (
+        <div variant="outlined">
+          <button
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            onClick={confirmAction}
+          >
+            {confirmText}
+          </button>
+          <button
+            className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => setConfirm(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      )}
+    </>
+  );
+}
--- frontend/src/routes/AdminHome.jsx
+++ frontend/src/routes/AdminHome.jsx
@@ -1,5 +1,11 @@
+import ConfirmButton from "../components/private/ConfirmButton";
 import BookCard from "../components/public/BookCard";
 
 export default function AdminHome() {
-  return <BookCard />;
+  return (
+    <>
+      <BookCard />
+      <ConfirmButton confirmText="Really Delete?" initialText="delete" />
+    </>
+  );
 }
